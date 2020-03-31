5f3f746530934505477e1f4b5514ad1ef3be016c
Patrick Hieber <patrickhieber92@gmail.com>
2020-03-31 18:02:08 +0200

    (feat) Authentication using AWS SecretsManager

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,13 +1,24 @@
 import { CustomAuthorizerHandler, CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
+import { verify } from 'jsonwebtoken';
+import { JwtToken } from '../../auth/JwtToken';
+
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+const client = new AWS.SecretsManager();
+
+let cachedSecret: string;
 
 export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
-    verifyToken(event.authorizationToken);
+    const decodedToken = await verifyToken(event.authorizationToken);
 
     console.log('User was authorized');
 
     return {
-      principalId: 'user',
+      principalId: decodedToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -38,7 +49,7 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
   }
 }
 
-function verifyToken(authHeader: string) {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
   if (!authHeader) {
     throw new Error('No authorization header');
   }
@@ -50,7 +61,20 @@ function verifyToken(authHeader: string) {
   const split = authHeader.split(' ');
   const token = split[1];
 
-  if (token !== '123') {
-    throw new Error('Invalid token');
-  }
+  const secretObject: any = await getSecret();
+  const secret = secretObject[secretField];
+
+  return verify(token, secret) as JwtToken;
+}
+
+async function getSecret() {
+  if (cachedSecret) return cachedSecret;
+
+  const data = await client.getSecretValue({
+    SecretId: secretId
+  }).promise();
+
+  cachedSecret = data.SecretString;
+
+  return JSON.parse(cachedSecret);
 }
