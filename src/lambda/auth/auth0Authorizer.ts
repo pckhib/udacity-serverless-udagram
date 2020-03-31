a538ff41726a2552d317d8f0ef6c7e5d6d39479c
Patrick Hieber <patrickhieber92@gmail.com>
2020-03-31 14:23:42 +0200

    (feat) Mock Authentication
    Implement a mocked authentication which will be used for further
    implementations.

---
+++ src/lambda/auth/auth0Authorizer.ts
@@ -0,0 +1,56 @@
+import { CustomAuthorizerHandler, CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
+
+export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+  try {
+    verifyToken(event.authorizationToken);
+
+    console.log('User was authorized');
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    console.log('User was not authorized', e.message);
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+function verifyToken(authHeader: string) {
+  if (!authHeader) {
+    throw new Error('No authorization header');
+  }
+
+  if (!authHeader.toLocaleLowerCase().startsWith('bearer ')) {
+    throw new Error('Invalid authorization header');
+  }
+
+  const split = authHeader.split(' ');
+  const token = split[1];
+
+  if (token !== '123') {
+    throw new Error('Invalid token');
+  }
+}
